8711008c804e904f11dd8d20b83539482b6fb68d
gazpacho <gazpachoblogesp@gmail.com>
2026-01-23 01:02:27 +0100

    Updates

--- 404.js
+++
@@ -1,8 +0,0 @@
-// GitHub Pages SPA redirect
-var path = window.location.pathname;
-var repo = "/mini-app-bruno";
-
-if (path.startsWith(repo)) {
-  var newPath = repo + "/?redirect=" + encodeURIComponent(path.replace(repo, ""));
-  window.location.replace(newPath);
-}
